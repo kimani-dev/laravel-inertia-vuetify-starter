f6fe36dae376d9743c1086169c7ed55b637d8ef7
David Kimani <daviemim07@gmail.com>
2024-05-06 15:43:16 +0300

    feature cards animation

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { Head } from "@inertiajs/vue3";
+import gsap from "gsap";
 
 defineProps({
     canLogin: Boolean,
@@ -7,6 +8,43 @@ defineProps({
     laravelVersion: String,
     phpVersion: String,
 });
+
+const features = [
+    {
+        title: "Pre-built & Customizable UI with Vuetify",
+        subtitle: "Beautiful Design Out of the Box",
+        icon: "mdi-vuetify",
+        text: "Benefit from a comprehensive library of Vuetify components that adhere to Material Design principles. Effortlessly create a modern and visually appealing user interface for your SaaS application, with extensive customization options to match your brand identity.",
+    },
+    {
+        title: "Robust Authentication & Authorization",
+        subtitle: "Secure Your Application with Granular Control",
+        icon: "mdi-lock",
+        text: "Implement robust user authentication with role-based permissions using our built-in features. Define user roles and assign granular permissions to control access to specific functionalities within your SaaS application, ensuring data security and a seamless user experience.",
+    },
+    {
+        title: "Powerful & Flexible CRUD System",
+        subtitle: "Manage CRUD Operations with Ease",
+        icon: "mdi-database",
+        text: "Skip building custom logic for Create, Read, Update, and Delete (CRUD) operations. Our template provides a powerful and adaptable CRUD layout that streamlines data management within your SaaS application. Simply define the routes and customize creation logic for your specific data models.",
+    },
+    {
+        title: "User Logs & Activity Tracking",
+        subtitle: "Monitor User Actions and System Events",
+        icon: "mdi-history",
+        text: "Comes with a built-in user activity log to track user actions and system events within your SaaS application. Monitor user interactions, system events, and application logs to gain insights into user behavior, troubleshoot issues, and ensure data integrity and security.",
+    },
+];
+
+// transition with stagger effect
+function onEnter(el, done) {
+    gsap.from(el, {
+        duration: 1,
+        opacity: 0,
+        delay: el.dataset.index * 0.15,
+        onComplete: done,
+    });
+}
 </script>
 
 <template>
@@ -16,6 +54,7 @@ defineProps({
         src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
         gradient="to bottom, rgba(0,0,0,.4), rgba(0,0,0,.7)"
         class="align-center text-white"
+        @loaded="console.log('loaded')"
     >
         <!-- Hero text start -->
         <h1 class="text-h3 text-center text-capitalize">
@@ -60,79 +99,27 @@ defineProps({
 
         <!-- Features start -->
         <v-row justify="center" class="mt-10">
-            <v-col cols="12" md="5">
-                <v-card
-                    title="Pre-built & Customizable UI with Vuetify"
-                    subtitle=" Beautiful Design Out of the Box"
-                    append-icon="mdi-vuetify"
-                    theme="dark"
-                    variant="tonal"
+            <TransitionGroup :css="false" @enter="onEnter" appear="">
+                <v-col
+                    cols="12"
+                    md="5"
+                    v-for="(feature, index) in features"
+                    :key="index"
+                    :data-index="index"
                 >
-                    <template #text>
-                        Benefit from a comprehensive library of Vuetify
-                        components that adhere to Material Design principles.
-                        Effortlessly create a modern and visually appealing user
-                        interface for your SaaS application, with extensive
-                        customization options to match your brand identity.
-                    </template>
-                </v-card>
-            </v-col>
-            <v-col cols="12" md="5">
-                <v-card
-                    title="Robust Authentication & Authorization"
-                    subtitle="Secure Your Application with Granular Control"
-                    append-icon="mdi-lock"
-                    theme="dark"
-                    variant="tonal"
-                >
-                    <template #text>
-                        Implement robust user authentication with role-based
-                        permissions using our built-in features. Define user
-                        roles and assign granular permissions to control access
-                        to specific functionalities within your SaaS
-                        application, ensuring data security and a seamless user
-                        experience.
-                    </template>
-                </v-card>
-            </v-col>
-        </v-row>
-        <v-row justify="center" class="mt-3">
-            <v-col cols="12" md="5">
-                <v-card
-                    title="Powerful & Flexible CRUD System"
-                    subtitle="Manage CRUD Operations with Ease"
-                    append-icon="mdi-database"
-                    theme="dark"
-                    variant="tonal"
-                >
-                    <template #text>
-                        Skip building custom logic for Create, Read, Update, and
-                        Delete (CRUD) operations. Our template provides a
-                        powerful and adaptable CRUD layout that streamlines data
-                        management within your SaaS application. Simply define
-                        the routes and customize creation logic for your
-                        specific data models.
-                    </template>
-                </v-card>
-            </v-col>
-            <v-col cols="12" md="5">
-                <v-card
-                    title="User Logs & Activity Tracking"
-                    subtitle="Monitor User Actions and System Events"
-                    append-icon="mdi-history"
-                    theme="dark"
-                    variant="tonal"
-                >
-                    <template #text>
-                        Comes with a built-in user activity log to track user
-                        actions and system events within your SaaS application.
-                        Monitor user interactions, system events, and
-                        application logs to gain insights into user behavior,
-                        troubleshoot issues, and ensure data integrity and
-                        security.
-                    </template>
-                </v-card>
-            </v-col>
+                    <v-card
+                        :title="feature.title"
+                        :subtitle="feature.subtitle"
+                        :append-icon="feature.icon"
+                        theme="dark"
+                        variant="tonal"
+                    >
+                        <template #text>
+                            {{ feature.text }}
+                        </template>
+                    </v-card>
+                </v-col>
+            </TransitionGroup>
         </v-row>
         <!-- Features end -->
         <p class="text-center mt-12 text-grey-darken-2 text-capitalize">
